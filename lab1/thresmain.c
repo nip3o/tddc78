4a8709c346aef652bd88bf741907a7fc0ead0de3
Niclas Olofsson <n@niclasolofsson.se>
2013-04-16 15:36:39 +0200

    Switched thesmain implementation

--- lab1/thresmain.c
+++ lab1/thresmain.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <malloc.h>
 #include <time.h>
 #include "ppmio.h"
 #include "thresfilter.h"
@@ -9,116 +10,93 @@
 #define ROOT 0
 
 int main (int argc, char ** argv) {
-    int rank, numprocs;
-
-    //-- Initialize MPI
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    int xsize, ysize, colmax;
+    int xsize, ysize, colmax, taskid, ntasks;
+    pixel src[MAX_PIXELS];
     struct timespec stime, etime;
 
-    pixel pix;
-    MPI_Datatype pix_mpi;
-    // MPI type to commit
-    int block_lengths [] = { 1, 1, 1 };
-    // Lengths of type elements
-    MPI_Datatype block_types [] = { MPI_UNSIGNED_CHAR, MPI_UNSIGNED_CHAR, MPI_UNSIGNED_CHAR };
+    MPI_Datatype pixel_mpi;
+    MPI_Datatype type[3] = { MPI_UNSIGNED_CHAR, MPI_UNSIGNED_CHAR, MPI_UNSIGNED_CHAR };
+    int blocklen[3] = { 1, 1, 1 };
+    MPI_Aint start, disp[3];
+ 
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
+    MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
 
-    //Set types
-    MPI_Aint start, displ[3];
+    MPI_Address( &src[0], &start );
+    MPI_Address( &src[0].r, &disp[0] );
+    MPI_Address( &src[0].g, &disp[1] );
+    MPI_Address( &src[0].b, &disp[2] );
+ 
+    disp[0] -= start;
+    disp[1] -= start;
+    disp[2] -= start;
 
-    MPI_Address( &pix, &start );
-    MPI_Address( &pix.r, &displ[0] );
-    MPI_Address( &pix.g, &displ[1] );
-    MPI_Address( &pix.b, &displ[2] );
+    MPI_Type_create_struct(3, blocklen, disp, type, &pixel_mpi);
+    MPI_Type_commit(&pixel_mpi);
 
-    displ[0] -= start;
-    displ[1] -= start;
-    displ[2] -= start;
-
-    MPI_Type_struct( 3, block_lengths, displ, block_types, &pix_mpi );
-    MPI_Type_commit( &pix_mpi );
-
-    pixel src[MAX_PIXELS];
 
     /* Take care of the arguments */
+    if (taskid == ROOT) {
+        if (argc != 3) {
+    	fprintf(stderr, "Usage: %s infile outfile\n", argv[0]);
+    	exit(1);
+        }
 
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s infile outfile\n", argv[0]);
-        exit(1);
-    }
-
-    int num[numprocs];
-    int distances[numprocs];
-
-    if (rank == ROOT) {
         /* read file */
         if(read_ppm (argv[1], &xsize, &ysize, &colmax, (char *) src) != 0)
             exit(1);
 
         if (colmax > 255) {
-            fprintf(stderr, "Too large maximum color-component value\n");
-            exit(1);
+    	fprintf(stderr, "Too large maximum color-component value\n");
+    	exit(1);
         }
 
-        printf("File initialized\n");
-
-        int i;
-        for (i = 0; i < numprocs; i++) {
-            num[i] = 1000;
-            distances[i] = i * numprocs;
-        }
-
-        printf("Generated problem sizes\n");
     }
 
-    pixel *recvbuffer = (pixel*) malloc(1000 * sizeof(pixel));
+    pixel recvbuff[MAX_PIXELS];
 
-    if(recvbuffer == 0) {
-       printf("Allocation failed");
-    } else {
-       printf("Mallocated %i bytes\n", (int) (1000 * sizeof(pixel)));
-    }
+    int buffsize = sizeof(src) / sizeof(pixel) / ntasks;
 
-    printf("Scattering");
+    MPI_Scatter(src, buffsize, pixel_mpi, 
+                recvbuff, buffsize, pixel_mpi,
+                0, MPI_COMM_WORLD);
 
-    MPI_Scatter( src, 1000, pix_mpi, rbuf, 1000, pix_mpi, ROOT, MPI_COMM_WORLD);
-    //MPI_Scatterv(src, num, distances, pix_mpi, recvbuffer, numprocs,
-    //             pix_mpi, ROOT, MPI_COMM_WORLD);
+//MPI_Scatter(sendbuff[0],buffsize,MPI_DOUBLE,
+//                   recvbuff,buffsize,MPI_DOUBLE,
+//                   0,MPI_COMM_WORLD);
+
+//MPI_Gather(&buffsum,1,MPI_DOUBLE,
+//                   buffsums,1, MPI_DOUBLE,
+//                   0,MPI_COMM_WORLD);
+
 
 
     printf("Has read the image, calling filter\n");
 
     clock_gettime(CLOCK_REALTIME, &stime);
 
-    thresfilter(100, 10, recvbuffer);
+    thresfilter(400, 400, src);
 
     clock_gettime(CLOCK_REALTIME, &etime);
 
+    MPI_Gather(src, buffsize, pixel_mpi,
+               recvbuff, buffsize, pixel_mpi,
+               0, MPI_COMM_WORLD);
 
-    printf("Gathering");
+    if (taskid == ROOT) {
+        printf("Filtering took: %g secs\n", (etime.tv_sec  - stime.tv_sec) +
+    	   1e-9*(etime.tv_nsec  - stime.tv_nsec)) ;
+
+        /* write result */
+        printf("Writing output file\n");
+        
+        if(write_ppm (argv[2], xsize, ysize, (char *)recvbuff) != 0)
+          exit(1);
+    } 
 
 
-    pixel* rbuf = (int *)malloc(numprocs * 1000 * sizeof(pix_mpi));
-    MPI_Gather( src, 1000, pix_mpi, rbuf, 1000, pix_mpi, ROOT, MPI_COMM_WORLD);
-
-    //MPI_Gatherv(recvbuffer, 1000, pix_mpi, src, num, distances,
-    //            pix_mpi, ROOT, MPI_COMM_WORLD);
-
-    //-- Terminate MPI.
     MPI_Finalize();
-
-    printf("Filtering took: %g secs\n", (etime.tv_sec  - stime.tv_sec) +
-       1e-9*(etime.tv_nsec  - stime.tv_nsec)) ;
-
-    /* write result */
-    printf("Writing output file\n");
-
-    if(write_ppm (argv[2], xsize, ysize, (char *)src) != 0)
-      exit(1);
-
 
     return(0);
 }
