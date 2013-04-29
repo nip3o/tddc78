6d75b07e8525d6be2d89b1a75df6e1f625ab3c23
Niclas Olofsson <n@niclasolofsson.se>
2013-04-29 10:28:26 +0200

    Spaces goddammnit

--- lab3/laplsolv.f90
+++ lab3/laplsolv.f90
@@ -30,13 +30,13 @@ program laplsolv
      error=0.0D0
      
      do j=1,n
-        tmp2=T(1:n,j)
-        T(1:n,j)=(T(0:n-1,j)+T(2:n+1,j)+T(1:n,j+1)+tmp1)/4.0D0
-        error=max(error,maxval(abs(tmp2-T(1:n,j))))
-        tmp1=tmp2
+        tmp2 = T(1:n, j)
+        T(1:n, j) = ( T(0:n - 1, j) + T(2:n+1, j) + T(1:n, j + 1) + tmp1) / 4.0D0
+        error = max(error, maxval(abs(tmp2 - T(1:n, j))))
+        tmp1 = tmp2
      end do
      
-     if (error<tol) then
+     if (error < tol) then
         exit
      end if
      
@@ -44,8 +44,8 @@ program laplsolv
   
   call cpu_time(t1)
 
-  write(unit=*,fmt=*) 'Time:',t1-t0,'Number of Iterations:',k
-  write(unit=*,fmt=*) 'Temperature of element T(1,1)  =',T(1,1)
+  write(unit=*, fmt=*) 'Time:', t1 - t0, 'Number of Iterations:', k
+  write(unit=*, fmt=*) 'Temperature of element T(1,1)  = ', T(1, 1)
 
   ! Uncomment the next part if you want to write the whole solution
   ! to a file. Useful for plotting. 
